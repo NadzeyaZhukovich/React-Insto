c362a07530f48d0658e6f734155aa316d097ac01
Nadzeya <naduva0505@gmail.com>
2019-06-26 15:25:37 +0200

    Added hooks in component Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,35 +1,47 @@
 import './Counter.scss';
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class Counter extends Component {
-    constructor(props) {
-        super(props);
+// export class Counter extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = {
-            counter: 0,
-        };
-    }
+//         this.state = {
+//             counter: 0,
+//         };
+//     }
 
-    handelButtonClick = (event) => {
-        const type = event.target.name;
-        let addition = -1;
+//     handelButtonClick = (event) => {
+//         const type = event.target.name;
+//         let addition = -1;
 
-        if (type === 'plus') {
-            addition = 1;
-        }
-        this.setState((prevState) => ({
-            counter: prevState.counter + addition,
-        }));
-    }
+//         if (type === 'plus') {
+//             addition = 1;
+//         }
+//         this.setState((prevState) => ({
+//             counter: prevState.counter + addition,
+//         }));
+//     }
 
-    render() {
-        return (
-            <div>
-                <button name="minus" onClick={this.handelButtonClick}>-</button>
-                {this.state.counter}
-                <button name="plus" onClick={this.handelButtonClick}>+</button>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <button name="minus" onClick={this.handelButtonClick}>-</button>
+//                 {this.state.counter}
+//                 <button name="plus" onClick={this.handelButtonClick}>+</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+export function Counter() {
+    const [count, setCount] = useState(0);
+    return(
+        <div>
+            {count > 0 && <button onClick={() => setCount(count - 1)}>-</button>}
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    );
 }
