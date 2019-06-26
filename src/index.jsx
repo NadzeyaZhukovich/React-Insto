244835c05456e00fb6576648ea3daaac648515ad
Nadzeya <naduva0505@gmail.com>
2019-06-26 23:21:11 +0200

    Added server and Timer component

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,16 @@ import ReactDom from 'react-dom';
 import { ProfileInfo } from './components/ProfileInfo';
 import { Gallery } from './components/Gallery';
 import { Counter } from './components/Counter';
+import { Timer } from './components/Timer';
 
 
 class App extends Component {
     render() {
         return (
             <main>
+                <div>
+                    <Timer/>
+                </div>
                 <div>
                     <Counter/>
                 </div>
